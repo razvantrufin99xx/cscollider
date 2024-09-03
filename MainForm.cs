30545e37156346830cc2796c86c676e37c2af46b
sibiu <144681144+razvantrufin99xx@users.noreply.github.com>
2024-09-03 12:23:06 +0300

    Add files via upload

---
+++ MainForm.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: razvan
+ * Date: 9/3/2024
+ * Time: 12:15 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace collider
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		private static double GetDistance(double x1, double y1, double x2, double y2)
+		{
+		   return Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+		}
+		
+		public bool collideDots(int x1,int y1,int x2, int y2, int r)
+		{
+			if(x1==x2 && y1==y2){return true;}
+			else{
+				if(GetDistance(x1,y1,x2,y2)<r)
+					{
+						return true;
+					}
+					else 
+					{
+						return false;
+					}
+				}
+			
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: razvan
+ * Date: 9/3/2024
+ * Time: 12:15 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Windows.Forms;
+
+namespace collider
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+		
+	}
+}
